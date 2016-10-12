71ad7884758b7838c224bfd2170255a7694b0973
bsevgeny@gmail.com <papakita2009>
2016-10-12 15:37:25 +0300

    end of scala

--- src/main/java/lab1/Main.scala
+++ src/main/java/lab1/Main.scala
@@ -1,7 +1,7 @@
 package lab1
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.{Accumulator, SparkConf, SparkContext}
 
 /**
   * Created by Evegeny on 12/10/2016.
@@ -12,6 +12,25 @@ object Main {
     val conf: SparkConf = new SparkConf().setAppName("taxi").setMaster("local[*]")
     val sc: SparkContext = new SparkContext(conf)
     val rdd: RDD[String] = sc.textFile("data/taxi/trips.txt")
-    rdd.foreach(println)
+    val driverTrips: RDD[(String, Int)] = rdd.map(line => {
+      val arr = line.split(" ")
+      (arr(0), arr(2).toInt)
+    })
+
+    val bigTrips: Accumulator[Int] = sc.accumulator(0,"big trips")
+    driverTrips.foreach(tuple=>{
+     if(tuple._2>10) bigTrips+=1
+    })
+
+    println(s"big trips is:  ${bigTrips.value}")
+    val take: Array[(String, Int)] = driverTrips.reduceByKey(_+_).sortBy(_._2,ascending = false).take(3)
+    val rdd1: RDD[(String, Int)] = sc.parallelize(take)
+    val rddDrivers: RDD[String] = sc.textFile("data/taxi/drivers.txt")
+    val rdd2: RDD[(String, String)] = rddDrivers.map(line => {
+      val strings: Array[String] = line.split(",")
+      (strings(0), strings(1))
+    })
+    rdd2.join(rdd1).foreach(println)
+
   }
 }
