52319b5d364e48c858e2aa76702bac11fb3ebfc2
bsevgeny@gmail.com <papakita2009>
2016-10-14 01:32:34 +0300

    kryo

--- src/main/java/songs/config/Config.java
+++ src/main/java/songs/config/Config.java
@@ -2,6 +2,7 @@ package songs.config;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SQLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,5 +23,10 @@ public class Config {
     @Bean
     public JavaSparkContext sc() {
         return new JavaSparkContext(sparkConf);
+    }
+
+    @Bean
+    public SQLContext sqlContext(){
+        return new SQLContext(sc());
     }
 }
