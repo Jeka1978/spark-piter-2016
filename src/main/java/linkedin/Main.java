747336aa2e9418803125920e3a84a1e6b448b729
bsevgeny@gmail.com <papakita2009>
2016-10-13 14:41:52 +0300

    football lab

--- src/main/java/linkedin/Main.java
+++ src/main/java/linkedin/Main.java
@@ -2,10 +2,9 @@ package linkedin;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.DataFrame;
+import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.functions;
 import org.apache.spark.sql.types.StructField;
 
 import java.util.Arrays;
@@ -21,11 +20,11 @@ public class Main {
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("linkedIn");
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
         SQLContext sqlContext = new SQLContext(sc);
-        DataFrame frame = sqlContext.read().json("data/linkedIn/*.json");
+        Dataset<Row> frame = sqlContext.read().json("data/linkedIn/*.json");
         frame.show();
         frame.printSchema();
         Arrays.stream(frame.schema().fields()).map(StructField::dataType).forEach(System.out::println);
-        Row[] collect = frame.collect();
+        Row[] collect = (Row[]) frame.collect();
         String[] columns = frame.columns();
         for (Row row : collect) {
             for (String column : columns) {
@@ -35,12 +34,12 @@ public class Main {
 
         frame = frame.withColumn("salary", col("age").multiply(size(col("keywords"))).multiply(10));
         frame.show();
-        Row rows = frame.select(explode(col("keywords")).as("keyword")).
+       /* Row rows = frame.select(explode(col("keywords")).as("keyword")).
                 groupBy("keyword").count().sort(col("count").desc()).take(1)[0];
         String mostPopular = rows.getAs("keyword").toString();
         System.out.println("mostPopular = " + mostPopular);
 
-        frame.where(array_contains(col("keywords"),mostPopular).and(col("salary").leq(1200))).show();
+        frame.where(array_contains(col("keywords"),mostPopular).and(col("salary").leq(1200))).show();*/
 
     }
 }
