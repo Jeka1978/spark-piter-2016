edb01afd0f1c3bf5a90b63d4696ae38e4e1f6d9b
bsevgeny@gmail.com <papakita2009>
2016-10-13 10:16:45 +0300

    garbage words added

--- src/main/java/songs/Main.java
+++ src/main/java/songs/Main.java
@@ -9,12 +9,13 @@ import songs.config.Config;
  * Created by Evegeny on 12/10/2016.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("hadoop.home.dir", "C:\\util\\hadoop-common-2.2.0-bin-master\\");
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
         TopXService service = context.getBean(TopXService.class);
         JavaSparkContext sc = context.getBean(JavaSparkContext.class);
         JavaRDD<String> rdd = sc.textFile("data/songs/beatles/*");
+        Thread.sleep(50000);
         service.topX(rdd,3).forEach(System.out::println);
     }
 }
